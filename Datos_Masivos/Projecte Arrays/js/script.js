8d4f2dff14e2a67db646a7cc6e9f03841f5f7cb1
angelusMZ19 <angelomontenegro200306@gmail.com>
2024-01-18 18:16:57 +0100

    avance

--- Datos_Masivos/Projecte Arrays/js/script.js
+++ Datos_Masivos/Projecte Arrays/js/script.js
@@ -1,81 +1,113 @@
-			
-// POKEMONS
 
 let dades;
-
+let pokemons=[];
+let municipis = [];
+let meteorits = [];
+let movies = [];
 
 // POKEMONS
 fetch("js/data/pokemon.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data.pokemon;		
-	
-	console.log(dades)
-	//console.log(dades[0].name)
-
-});
-
-
+  .then((response) => response.json())
+  .then((data) => {
+    pokemons = data.pokemon;
+  });
 
 // MUNICIPIS
 fetch("js/data/municipis.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data.elements;		
-	
-	console.log(dades)
-	//console.log(dades[0].municipi_nom)
-});
-
-
+  .then((response) => response.json())
+  .then((data) => {
+    municipis = data.elements;
+  });
 
 // METEORITS
 fetch("js/data/earthMeteorites.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data;		
-	
-	console.log(dades)
-	//console.log(dades[0].name)
-});
-
+  .then((response) => response.json())
+  .then((data) => {
+    meteorits = data;
+  });
 
 // MOVIES
 fetch("js/data/movies.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data.movies;		
+  .then((response) => response.json())
+  .then((data) => {
+    movies = data.movies;
+
+    /* if (pokemonsa && municipis && meteorits && movies) {
+      const combined = pokemons.map((pokemons, index) => {
+        return {
+          pokemon: pokemons.name,
+          municipi: municipis[index].municipi_nom,
+          earthMeteorite: meteorits[index].name,
+          movie: movies[index].title
+        };
+      });
+      console.table(combined);
+    }*/
+  });
+
+
+  //////////////////////////////////////////////////////////////////////////////////
+  function creaTabla() {
+    console.clear();
+    let tabla = [];
+    for (let i = 0; i < 1000; i++) {
+      let dades = {
+         pokemons[i],
+         municipis[i],
+         meteorits[i],
+         movies[i]
+      };
+      tabla.push(dades);
+    }
+    document.getElementById("tabla").innerHTML = tabla;
+    return tabla;
+    console.table(tabla);
+  }
+  
+  function refresca() {
+    location.reload();
+  }
+
+
+  function orderListAsc() {
+    pokemons.sort();
+    municipis.sort();
+    meteorits.sort();
+    movies.sort();
+    creaTabla();
+}
+
+function orderListDesc() {
+  pokemons.reverse();
+  municipis.reverse();
+  meteorits.reverse();
+  movies.reverse();
+  creaTabla();
+}
+
+
+function searchList() {
+    const searchTerm = prompt("Introdueix el valor a buscar:");
+    const resultIndex = pokemons.indexOf(parseInt(searchTerm));
+    
+    if (resultIndex !== -1) {
+        alert("Valor trobat a la posició " + resultIndex);
+    } else {
+        alert("Valor no trobat en l'array");
+    }
+}
+
+function calcMitjana() {
+	let mitjana = 0;
+	getDades().forEach(dada => {
+		
+		if (dada[3] != undefined) mitjana += parseInt(dada[3]);
+	});
+	mitjana /= getDades().length;
+	mitjana = mitjana.toFixed(2);
 	
-	console.log(dades)
-	//console.log(dades[0].title)
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var arrayDeArrays = [];
-var fila = 0
-jsonparsed.forEach(function(objeto){
-arrayDeArrays[fila] = [];
-arrayDeArrays[fila].push(pokemon.nom);
-arrayDeArrays[fila].push(municipis.municipi);
-arrayDeArrays[fila].push(objeto.peliculas);
-arrayDeArrays[fila].push(objeto.earthmeteorit);
-fila +=1;
-   }
-); */
+	let p = document.getElementById('mitjana');
+	if (getRadioButton() == 'pokemons' || getRadioButton()=='meteorits') p.innerHTML=mitjana+' kg';
+	if (getRadioButton() == 'municipis')  p.innerHTML = mitjana + ' habitants';
+	if (getRadioButton() == 'movies') p.innerHTML = mitjana + ' punts';
+}
